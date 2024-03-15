c8b76bc9bd600a5342048370d1fcd55a1e186dda
Chandan Kumar <ck22abf@herts.ac.uk>
2024-03-15 19:20:27 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Create End to End project
+
+## Create a virtual Environment
+```
+conda create -p venv python=3.8
+conda activate venv/
+```
+
+### Install all the libraries
+
+```
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_Dot = "-e ."
+
+def get_requirements(file_path:str) ->List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_Dot in requirements:
+            requirements.remove(HYPHEN_E_Dot)
+
+    return requirements
+
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    description='Linear,Ridge,Lasso,ElasticNet Implementation',
+    author='Chandan Kumar',
+    author_email='chandankumarsingh1351@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
